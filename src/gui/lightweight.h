a99ca0ef79bc148cce145ab147979d0e4e21c23f
nyanpasu64 <nyanpasu64@tuta.io>
2019-07-16 01:27:22 -0700

    Dedent defer{}; definition to appease Qt Creator

--- src/gui/lightweight.h
+++ src/gui/lightweight.h
@@ -2,27 +2,27 @@
 
 #ifndef defer
 
-    // https://stackoverflow.com/a/42060129
-    struct DeferDummy {};
+// https://stackoverflow.com/a/42060129
+struct DeferDummy {};
 
-    template <class Lambda>
-    struct Deferrer {
-        Lambda f;
-        ~Deferrer() { f(); }
-    };
+template <class Lambda>
+struct Deferrer {
+    Lambda f;
+    ~Deferrer() { f(); }
+};
 
-    template <class Lambda>
-    Deferrer<Lambda> operator<<(DeferDummy, Lambda f) {
-        return {f};
-    }
+template <class Lambda>
+Deferrer<Lambda> operator<<(DeferDummy, Lambda f) {
+    return {f};
+}
 
-    #define DEFER_(LINE) zz_defer##LINE
-    #define DEFER(LINE) DEFER_(LINE)
-    #define defer \
-        auto DEFER(__LINE__) = DeferDummy{} << [&]()
+#define DEFER_(LINE) zz_defer##LINE
+#define DEFER(LINE) DEFER_(LINE)
+#define defer \
+    auto DEFER(__LINE__) = DeferDummy{} << [&]()
 
-    // Usage:
-    // defer {...};
+// Usage:
+// defer {...};
 
 #endif // defer
 
