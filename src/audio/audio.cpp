cdc8eeb094f74f1300fb1486ce958c2513d0317a
nyanpasu64 <nyanpasu64@tuta.io>
2019-10-03 02:35:44 -0700

    Remove audio/synth.*
    There is no longer a synth thread.
    Maybe there's a separate synth object, but these files are unused so far.

--- src/audio/audio.cpp
+++ src/audio/audio.cpp
@@ -1,6 +1,5 @@
 #include "audio.h"
 #include "audio/output.h"
-#include "audio/synth.h"
 
 namespace audio {
 
--- src/audio/synth.cpp
+++
@@ -1,9 +0,0 @@
-#include "synth.h"
-
-namespace audio {
-namespace synth {
-
-const static int WAIT_FOR_AUDIO_WRITE_SEC = 2;
-
-}
-}
