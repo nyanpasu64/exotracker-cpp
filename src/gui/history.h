188911781acf0891afb7e80721260911270c766f
nyanpasu64 <nyanpasu64@tuta.io>
2019-10-04 21:24:42 -0700

    Move History methods to history.cpp

--- src/gui/history.h
+++ src/gui/history.h
@@ -44,50 +44,16 @@ public:
     ///
     /// But this is unlikely, since the main thread does not delete `current`
     /// but instead moves it to undo/redo (and redo is only cleared upon new actions).
-    BoxT const get() const {
-        return current.load();
-    }
+    BoxT const get() const;
 
     /// Called from UI thread. Clear redo stack and push a new element into the history.
-    void push(BoxT item) {
-        // Do I add support for tree-undo?
-        redo_stack.clear();
-
-        // TODO if HistoryFrame.can_merge(current.load()), store to current and discard old value.
-
-        // Move new state into current. Move current state into undo stack.
-        undo_stack.emplace_back(current.exchange(item));
-    }
+    void push(BoxT item);
 
     /// Called from UI thread. Switch `get()` to previous state.
-    Success undo() {
-        if (undo_stack.empty()) {
-            return Success{false};
-        }
-
-        // Pop undo into current state. Move current state into redo.
-        redo_stack.emplace_back(current.exchange(undo_stack.back()));
-
-        // vector.pop_back() returns void because it's impossible to return the object exception-safely.
-        // https://stackoverflow.com/a/12600477
-        // I think "dealing with other people's exceptions" is painful.
-        undo_stack.pop_back();
-
-        return Success{true};
-    }
+    Success undo();
 
     /// Called from UI thread. Switch `get()` to next state.
-    Success redo() {
-        if (redo_stack.empty()) {
-            return Success{false};
-        }
-
-        // Pop redo into current state. Move current state into undo.
-        undo_stack.emplace_back(current.exchange(redo_stack.back()));
-        redo_stack.pop_back();
-
-        return Success{true};
-    }
+    Success redo();
 };
 
 // namespace history
