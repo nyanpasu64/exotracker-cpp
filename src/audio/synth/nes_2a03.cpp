01ec87d14ee0abe7b0da30ea7c0f39c714043d83
nyanpasu64 <nyanpasu64@tuta.io>
2019-10-25 23:05:35 -0700

    Add nsfplay to Nes2A03Synth (APU1, APU2, and CPU)

--- src/audio/synth/nes_2a03.cpp
+++ src/audio/synth/nes_2a03.cpp
@@ -1,13 +1,47 @@
 #include "nes_2a03.h"
 
+#include <array>
+
 namespace audio {
 namespace synth {
 namespace nes_2a03 {
 
+const static int APU1_RANGE = 100;
+const static int APU2_RANGE = 100;
+
+const static double APU1_VOLUME = 0.0;
+const static double APU2_VOLUME = 0.0;
+
+Nes2A03Synth::Nes2A03Synth(Blip_Buffer &blip) :
+    apu1_synth{blip, APU1_RANGE, APU1_VOLUME},
+    apu2_synth{blip, APU2_RANGE, APU2_VOLUME}
+{
+    apu1.Reset();
+    apu2.Reset();
+
+    apu2.SetCPU(&cpu);
+    apu2.SetAPU(&apu1);
+}
 
 NesChipSynth::SynthResult Nes2A03Synth::synthesize_chip_clocks(
         ClockT nclk, gsl::span<Amplitude> write_buffer
         ) {
+    std::array<INT32, 2> stereo_out;
+
+    // Will running apu1 and apu2 in separate loops improve locality of reference?
+    for (ClockT clock = 0; clock < nclk; clock++) {
+        apu1.Tick(1);
+        apu1.Render(&stereo_out[0]);
+        apu1_synth.update((blip_nclock_t) clock, stereo_out[0]);
+    }
+
+    // Make sure there are no copy-paste errors where you mix up apu1 and apu2 ;)
+    for (ClockT clock = 0; clock < nclk; clock++) {
+        apu2.Tick(1);
+        apu2.Render(&stereo_out[0]);
+        apu2_synth.update((blip_nclock_t) clock, stereo_out[0]);
+    }
+
     return SynthResult{.wrote_audio=false, .nsamp_returned=0};
 }
 
--- src/audio/synth/nes_2a03.h
+++ src/audio/synth/nes_2a03.h
@@ -2,14 +2,27 @@
 
 #include "../synth_common.h"
 
+#include <nsfplay/xgm/devices/CPU/nes_cpu.h>
+#include <nsfplay/xgm/devices/Sound/nes_apu.h>
+#include <nsfplay/xgm/devices/Sound/nes_dmc.h>
+
 namespace audio {
 namespace synth {
 namespace nes_2a03 {
 
 class Nes2A03Synth : public NesChipSynth {
+    xgm::NES_CPU cpu;
+
+    /// APU1 (2 pulses)
+    xgm::NES_APU apu1;
+    /// APU2 (tri noise dpcm)
+    xgm::NES_DMC apu2;
+
+    MyBlipSynth apu1_synth;
+    MyBlipSynth apu2_synth;
 
 public:
-    explicit Nes2A03Synth(Blip_Buffer & blip) {}
+    explicit Nes2A03Synth(Blip_Buffer & blip);
 
     // impl NesChipSynth
     SynthResult synthesize_chip_clocks(
