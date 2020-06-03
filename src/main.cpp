4db1de8ce4688a6be63ae79eb934bc6dab4caf2c
nyanpasu64 <nyanpasu64@tuta.io>
2020-06-03 11:30:58 -0700

    Parse command-line arguments using CLI11

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include "audio.h"
 #include "sample_docs.h"
 
+#include <CLI/CLI.hpp>
 #include <fmt/core.h>
 #include <rtaudio/RtAudio.h>
 
 #include <QApplication>
 
 #include <iostream>
+#include <variant>
 
 #include "win32_fonts.h"
 
@@ -16,12 +18,61 @@ using std::unique_ptr;
 using fmt::print;
 using gui::MainWindow;
 
+
+struct ReturnCode {
+    int value;
+};
+
+struct Arguments {
+    std::string doc_name;
+
+    [[nodiscard]]
+    static std::variant<Arguments, ReturnCode> parse(int argc, char *argv[]) {
+        CLI::App app;
+
+        std::string doc_name = sample_docs::DEFAULT_DOC;
+
+        app.add_option("doc_name", /*mut*/ doc_name, "Name of sample document to load");
+        app.failure_message(CLI::FailureMessage::help);
+
+        try {
+            app.parse(argc, argv);
+        } catch (const CLI::ParseError &e) {
+            return ReturnCode{app.exit(e)};
+        }
+
+        return Arguments{.doc_name=doc_name};
+    }
+};
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     win32_set_font();
 
-    auto const & document = sample_docs::DOCUMENTS.at(sample_docs::DEFAULT_DOC);
+    // Parse command-line arguments.
+    auto maybe_arg = Arguments::parse(argc, argv);
+    if (auto return_code = std::get_if<ReturnCode>(&maybe_arg)) {
+        return return_code->value;
+    }
+    auto arg = std::get<Arguments>(std::move(maybe_arg));
+
+    using sample_docs::DOCUMENTS;
+    if (!DOCUMENTS.contains(arg.doc_name)) {
+        fmt::print(
+            stderr, "Invalid document name \"{}\". Valid names are:\n\n", arg.doc_name
+        );
+
+        std::vector<std::string> keys;
+        keys.reserve(DOCUMENTS.size());
+        for (auto && [doc_name, _] : DOCUMENTS) {
+            fmt::print(stderr, "  {}\n", doc_name);
+        }
+
+        return 1;
+    }
+    auto const & document = sample_docs::DOCUMENTS.at(arg.doc_name);
     gui::history::History history{document.clone()};
 
     RtAudio rt;
