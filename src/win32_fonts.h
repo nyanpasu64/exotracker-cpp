dd95cfbd417cb4c596bfd3eceb4f6fa59c999ce7
nyanpasu64 <nyanpasu64@tuta.io>
2020-07-10 18:21:09 -0700

    Switch from lfMenuFont to lfMessageFont

--- src/win32_fonts.h
+++ src/win32_fonts.h
@@ -8,11 +8,11 @@
 
 void win32_set_font() {
   // On Windows, Qt 5's default system font (MS Shell Dlg 2) is outdated.
-  // Interestingly, the QMenu font is correct and comes from lfMenuFont
+  // Interestingly, the QMessageBox font is correct and comes from lfMessageFont
   // (Segoe UI on English computers).
   // So use it for the entire application.
   // This code will become unnecessary and obsolete once we switch to Qt 6.
-  QApplication::setFont(QApplication::font("QMenu"));
+  QApplication::setFont(QApplication::font("QMessageBox"));
 }
 
 #else
