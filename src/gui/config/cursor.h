55f3e897aa7de6c8c7f1a364c038080d4a189048
nyanpasu64 <nyanpasu64@tuta.io>
2020-10-10 18:30:59 -0700

    Disable cursor/step snapping to events as well as gridlines

--- src/gui/config/cursor.h
+++ src/gui/config/cursor.h
@@ -11,7 +11,7 @@ struct MovementConfig {
     bool wrap_across_frames = true;
     bool home_end_switch_patterns = true;
     bool arrow_follows_step = true;
-    bool snap_to_events = true;
+    bool snap_to_events = false;
 
     BeatFraction page_down_distance{1};
 };
