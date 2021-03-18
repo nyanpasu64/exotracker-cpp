848cc12fd7718de53f0ddf5197014b4f24c99ffd
nyanpasu64 <nyanpasu64@tuta.io>
2021-03-17 19:13:04 -0700

    Remove nsfplay (doesn't compile)

--- 3rdparty/nsfplay/push_winamp.bat
+++
@@ -1,7 +0,0 @@
-cd /d "%~dp0"
-call distribute.bat
-copy distribute\plugins\*.* "C:\Program Files (x86)\Winamp\Plugins"
-@echo.
-@echo Review the log above for errors. This batch file should be run as administrator.
-@echo.
-@pause
