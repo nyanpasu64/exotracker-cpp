d6b08205eb27a5bdfede213b772b17a1a713e355
nyanpasu64 <nyanpasu64@tuta.io>
2021-05-03 16:59:05 -0700

    Restrict patterns to a maximum size of 64k events
    This limit is being added to protect serialization/deserialization, and
    also affects editing as well.

--- src/doc/event_list.h
+++ src/doc/event_list.h
@@ -15,6 +15,15 @@ using timed_events::TimedRowEvent;
 
 using EventIndex = uint32_t;
 
+/// The maximum number of events allowed in a single pattern during file loading,
+/// or at any point during editing.
+///
+/// 256 events is insufficient for very long patterns (like if you wanted to store an
+/// entire classical movement in a single pattern without breaks).
+///
+/// 65535 events is more than sufficient to store even pathological monophonic tracks.
+constexpr int MAX_EVENTS_PER_PATTERN = 0xffff;
+
 using EventList = std::vector<TimedRowEvent>;
 using TimedEventsRef = gsl::span<timed_events::TimedRowEvent const>;
 
