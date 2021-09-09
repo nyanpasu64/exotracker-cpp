02ea3d76e0317e7273fcaf98501fb8430d6f8809
nyanpasu64 <nyanpasu64@tuta.io>
2021-09-08 21:09:10 -0700

    Add drag-and-drop support for instrument list (no backend yet)

--- src/gui/instrument_list.cpp
+++ src/gui/instrument_list.cpp
@@ -13,29 +13,33 @@
 #include <QVBoxLayout>
 
 // Other
-#include <QSignalBlocker>
 #include <QAbstractListModel>
-#include <QSortFilterProxyModel>
+#include <QDebug>
+#include <QMimeData>
+#include <QSignalBlocker>
 
 namespace gui::instrument_list {
 W_OBJECT_IMPL(InstrumentList)
 
-namespace InstrumentListRole {
-enum : int {
-    Color = Qt::BackgroundRole,  // QBrush
-    IsEnabled = Qt::UserRole,  // bool
-};
-}
-
 using gui::lib::format::format_hex_2;
 
 namespace {
-class InstrumentListModel : public QAbstractListModel {
+enum class DragAction {
+    /// Dragging an instrument swaps the source and destination.
+    Swap,
+    /// Dragging an instrument moves the source into a gap between instruments (not
+    /// implemented yet).
+    Move,
+};
+
+class InstrumentListModel final : public QAbstractListModel {
     W_OBJECT(InstrumentListModel)
-public:
+private:
     GetDocument _get_document;
+    DragAction _drag_action = DragAction::Swap;
 
 // impl
+public:
     InstrumentListModel(GetDocument get_document)
         : _get_document(get_document)
     {}
@@ -50,75 +54,172 @@ public:
         endResetModel();
     }
 
-    // impl QAbstractListModel
-    int rowCount(QModelIndex const & /*parent*/) const override {
-        return int(get_document().instruments.v.size());
+// impl QAbstractItemModel
+public:
+    int rowCount(QModelIndex const & parent) const override {
+        if (parent.isValid()) {
+            // Rows do not have children.
+            return 0;
+        } else {
+            // The root has items.
+            return int(get_document().instruments.v.size());
+        }
     }
 
     QVariant data(QModelIndex const & index, int role) const override {
-        auto & x = get_document().instruments.v;
+        auto & instruments = get_document().instruments.v;
 
-        if (!index.isValid())
+        if (!index.isValid() || index.parent().isValid())
             return QVariant();
 
         auto row = (size_t) index.row();
-        if (row >= x.size())
+        if (row >= instruments.size())
             return QVariant();
 
         if (role == Qt::DisplayRole) {
-            if (x[row].has_value()) {
+            if (instruments[row].has_value()) {
                 return QStringLiteral("%1 - %2").arg(
-                    format_hex_2(row), QString::fromStdString(x[row]->name)
+                    format_hex_2(row), QString::fromStdString(instruments[row]->name)
                 );
             } else {
                 return format_hex_2(row);
             }
         }
-        if (role == InstrumentListRole::IsEnabled) {
-            return x[row].has_value();
-        }
 
         return QVariant();
+    }
+
+    /*
+    Qt drag and drop is byzantine, like CMake.
+
+    QAbstractItemView::startDrag() calls QDrag::exec(), which serializes the drag
+    origin into MIME and calls QListView::dropEvent() when you release the mouse.
+    When you drag an item between items, QListView::dropEvent() calls
+    InstrumentListModel::moveRows() and returns.
+
+    If you instead drag an item onto another item using Qt::MoveAction, Qt is designed
+    to overwrite the target with the source and then erase the source:
+    QListView::dropEvent() falls through to QAbstractItemView::dropEvent() which calls
+    InstrumentListModel::dropMimeData(). Afterwards, QAbstractItemView::startDrag()
+    calls InstrumentListModel::removeRows() to remove the origin of the drag.
+
+    However I want dragging an item onto another item to instead swap them.
+    I have two options:
+
+    - Qt::CopyAction is easy to work with, but requires QListView::DragDrop (which
+      allows users to drag items *between* unrelated widgets, in which case we
+      erroneously index into InstrumentListModel using an item dragged from an
+      unrelated model).
+    - QListView::InternalMove prevents cross-widget dragging, but doesn't support
+      Qt::CopyAction but only Qt::MoveAction, which sends a spurious removeRows() we
+      must ignore).
+
+    I decided to pick InternalMove because it's the easiest way to ensure local
+    reasoning.
+
+    ----
+
+    How do we control whether an item is dropped onto the nearest item (to swap),
+    onto the nearest gap between items (to move), or both? This is determined by
+    QAbstractItemViewPrivate::position():
+
+    - If QAIM::flags() called with a valid index omits Qt::ItemIsDropEnabled,
+      then QListView will only drag onto a gap between items.
+    - Otherwise, if QListWidget::setDragDropOverwriteMode(true) is called,
+      then QListView will only drag onto an item.
+    - If neither is the case, then QListView will allow both (which is hard for the
+      user to control because the gap between items is very thin).
+
+    My approach is to call setDragDropOverwriteMode(true) to ensure large hitboxes,
+    then use QAIM::flags() to control whether the user drags onto or between items.
+    */
+
+    Qt::ItemFlags flags(const QModelIndex &index) const override {
+        Qt::ItemFlags defaultFlags = QAbstractListModel::flags(index);
+
+        Qt::ItemFlags should_drop = _drag_action == DragAction::Swap
+            ? Qt::ItemIsDropEnabled
+            : Qt::ItemFlag(0);
+
+        if (index.isValid())
+            return Qt::ItemIsDragEnabled | should_drop | defaultFlags;
+        else
+            return Qt::ItemIsDropEnabled | defaultFlags;
+    }
+
+    Qt::DropActions supportedDragActions() const override {
+        return Qt::MoveAction;
+    }
+
+    Qt::DropActions supportedDropActions() const override {
+        return Qt::MoveAction;
+    }
+
+    // TODO when I add move-row support, override moveRows() and create an EditBox
+    // when called.
+
+    bool dropMimeData(
+        QMimeData const* data,
+        Qt::DropAction action,
+        int insert_row,
+        int insert_column,
+        QModelIndex const& replace_index)
+    override {
+        // Based off QAbstractListModel::dropMimeData().
+        if (!data || !(action == Qt::CopyAction || action == Qt::MoveAction))
+            return false;
+
+        QStringList types = mimeTypes();
+        if (types.isEmpty())
+            return false;
+        QString format = types.at(0);
+        if (!data->hasFormat(format))
+            return false;
+
+        QByteArray encoded = data->data(format);
+        QDataStream stream(&encoded, QIODevice::ReadOnly);
+
+        // if the drop is on an item, swap the dragged and dropped items.
+        if (replace_index.isValid() && insert_row == -1 && insert_column == -1) {
+            int drag_row;
+            int drag_column;
+            stream >> drag_row >> drag_column;
+
+            // TODO create an EditBox to swap the items.
+            qDebug() << drag_row << replace_index.row();
+            return true;
+        }
+
+        if (insert_row == -1)
+            insert_row = rowCount(replace_index);
+
+        // otherwise insert new rows for the data
+        return decodeData(insert_row, insert_column, replace_index, stream);
+    }
+
+    /// removeRows() is called by QAbstractItemView::startDrag() when the user drags
+    /// two items to swap them. But we want to swap items, not overwrite one with
+    /// another. So ignore the call.
+    bool removeRows(int row, int count, const QModelIndex & parent) override {
+        return false;
     }
 };
 W_OBJECT_IMPL(InstrumentListModel)
 
-class InstrumentListFilter : public QSortFilterProxyModel {
-    bool _show_empty_slots = false;
-
-public:
-    void show_empty_slots(bool show) {
-        if (show != _show_empty_slots) {
-            _show_empty_slots = show;
-            invalidateFilter();
-        }
-    }
-
-    // impl QSortFilterProxyModel
-    protected:
-    bool filterAcceptsRow(int source_row, const QModelIndex &) const override {
-        if (_show_empty_slots) {
-            return true;
-        }
-        return sourceModel()->data(
-            sourceModel()->index(source_row, 0), InstrumentListRole::IsEnabled
-        ).toBool();
-    }
-};
-
-class InstrumentListImpl : public InstrumentList {
+class InstrumentListImpl final : public InstrumentList {
     W_OBJECT(InstrumentListImpl)
 public:
     MainWindow & _win;
+    InstrumentListModel _model;
+    bool _show_empty_slots = false;
 
-    InstrumentListModel _source_model;
-    InstrumentListFilter _filter_model;
-    QListView * _widget;
+    // Widgets
+    QListView * _list;
 
     explicit InstrumentListImpl(MainWindow * win, QWidget * parent)
         : InstrumentList(parent)
         , _win(*win)
-        , _source_model(GetDocument::empty())
+        , _model(GetDocument::empty())
     {
         auto c = this;
         auto l = new QVBoxLayout(c);
@@ -126,56 +227,73 @@ public:
 
         l->setContentsMargins(0, 0, 0, 0);
 
-        {
-            l__w(QListView);
-            _widget = w;
+        {l__w(QListView);
+            _list = w;
             w->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
             w->setWrapping(true);
         }
 
-        // _filter_model holds a reference, does *not* take ownership.
-        // If filter is destroyed first, it doesn't affect the source.
-        // If source is destroyed first, its destroyed() signal disconnects the filter mode.
-        // _filter_model is destroyed before _source_model (comes after in the list of fields).
-        _filter_model.setSourceModel(&_source_model);
-
         // Widget holds a reference, does *not* take ownership.
         // If widget is destroyed first, it doesn't affect the model.
         // If model is destroyed first, its destroyed() signal disconnects all widgets using it.
-        _widget->setModel(&_filter_model);
+        _list->setModel(&_model);
+
+        _list->setDragEnabled(true);
+        _list->setAcceptDrops(true);
+
+        // See the comment in InstrumentListModel for an explanation of DragDropMode.
+        _list->setDragDropMode(QListView::InternalMove);
+        _list->setDragDropOverwriteMode(true);
+        _list->setDropIndicatorShown(true);
 
         connect(
-            _widget->selectionModel(), &QItemSelectionModel::selectionChanged,
-            win, [this, win](const QItemSelection &filter_sel, const QItemSelection &) {
+            _list->selectionModel(), &QItemSelectionModel::selectionChanged,
+            win, [win](const QItemSelection &selection, const QItemSelection &) {
                 // Only 1 element can be selected at once, or 0 if you ctrl+click.
-                assert(filter_sel.size() <= 1);
-                QItemSelection source_sel = _filter_model.mapSelectionToSource(filter_sel);
-                if (!source_sel.empty()) {
+                assert(selection.size() <= 1);
+                if (!selection.empty()) {
                     debug_unwrap(win->edit_state(), [&](auto & tx) {
-                        tx.set_instrument(source_sel[0].top());
+                        tx.set_instrument(selection[0].top());
                     });
                 }
             });
 
         connect(
-            _widget, &QListView::doubleClicked,
+            _list, &QListView::doubleClicked,
             this, &InstrumentListImpl::on_double_click);
     }
 
+    // it's a nasty hack that we set history to reload changes from a StateTransaction,
+    // but it works don't touch it
     void set_history(GetDocument get_document) override {
-        _source_model.set_history(get_document);
+        _model.set_history(get_document);
+        recompute_visible_slots();
         update_selection();
     }
 
-    void update_selection() override {
-        auto source_idx = _source_model.index(_win._state.instrument(), 0);
-        auto filter_idx = _filter_model.mapFromSource(source_idx);
+    void recompute_visible_slots() {
+        auto & instruments = _model.get_document().instruments.v;
+        int nrow = _model.rowCount({});
 
-        QItemSelectionModel & widget_select = *_widget->selectionModel();
-        // _widget->selectionModel() merely responds to the active instrument.
+        if (_show_empty_slots) {
+            for (int row = 0; row < nrow; row++) {
+                _list->setRowHidden(row, false);
+            }
+        } else {
+            for (int row = 0; row < nrow; row++) {
+                _list->setRowHidden(row, !instruments[(size_t) row].has_value());
+            }
+        }
+    }
+
+    void update_selection() override {
+        auto idx = _model.index(_win._state.instrument(), 0);
+
+        QItemSelectionModel & list_select = *_list->selectionModel();
+        // _list->selectionModel() merely responds to the active instrument.
         // Block signals when we change it to match the active instrument.
-        auto s = QSignalBlocker(widget_select);
-        widget_select.select(filter_idx, QItemSelectionModel::ClearAndSelect);
+        auto s = QSignalBlocker(list_select);
+        list_select.select(idx, QItemSelectionModel::ClearAndSelect);
 
         // Hack to avoid scrolling a widget before it's shown
         // (which causes broken layout and crashes).
@@ -184,11 +302,11 @@ public:
         // and even if it was nonzero, only the scrolling will be wrong,
         // not the actual selected instrument (which could cause a desync).
         if (isVisible()) {
-            _widget->scrollTo(filter_idx);
+            _list->scrollTo(idx);
         }
     }
 
-    void on_double_click(QModelIndex const& filter_idx) {
+    void on_double_click(QModelIndex const& /*idx*/) {
         _win.show_instr_dialog();
     }
 };
