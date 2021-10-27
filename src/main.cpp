b12042d288eccd7ef054f36c6d1a396aba760c6f
nyanpasu64 <nyanpasu64@tuta.io>
2021-10-26 21:19:58 -0700

    Rewrite command-line handling using QCommandLineParser
    QCommandLineParser supports Unicode unlike CLI11.

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 #include "sample_docs.h"
 #include "util/expr.h"
 
-#include <CLI/CLI.hpp>
 #include <fmt/core.h>
 
 #include <QApplication>
+#include <QCommandLineParser>
 
 #include <variant>
 
@@ -30,64 +30,152 @@ struct ReturnCode {
     int value;
 };
 
+// TODO put in header
+/// Translate a string in a global context, outside of a class.
+static QString gtr(
+    const char *context,
+    const char *sourceText,
+    const char *disambiguation = nullptr,
+    int n = -1)
+{
+    return QCoreApplication::translate(context, sourceText, disambiguation, n);
+}
+
+static bool has(std::string const& s) {
+    return !s.empty();
+}
+
+static bool has(QString const& s) {
+    return !s.isEmpty();
+}
+
+/// Returns the default help text, followed by a list of sample document names.
+QString help_text(QCommandLineParser & parser) {
+    auto text = parser.helpText();
+    text += QStringLiteral("\n%1\n%2").arg(
+        gtr("main", "Sample document names:"),
+        QString::fromStdString(list_documents()));
+    return text;
+}
+
+[[noreturn]] static void bail_only(QString error) {
+    fprintf(stderr, "%s\n", error.toUtf8().data());
+    exit(1);
+}
+
+[[noreturn]] static void bail_help(QCommandLineParser & parser, QString error) {
+    fprintf(stderr, "%s\n\n%s",
+        error.toUtf8().data(),
+        help_text(parser).toUtf8().data());
+    exit(1);
+}
+
+[[noreturn]] static void help_and_exit(QCommandLineParser & parser) {
+    fputs(help_text(parser).toUtf8().data(), stdout);
+    exit(0);
+}
+
 struct Arguments {
-    std::string doc_name;
+    QString filename;
+    std::string sample_doc;
 
+    /// May exit if invalid arguments, --help, or --version is passed.
     [[nodiscard]]
-    static std::variant<Arguments, ReturnCode> parse(int argc, char *argv[]) {
-        CLI::App app;
+    static Arguments parse_or_exit(QStringList const& arguments) {
+        using sample_docs::DOCUMENTS;
 
-        std::string doc_name;
+        QCommandLineParser parser;
 
-        app.add_option("doc_name", /*mut*/ doc_name, "Name of sample document to load");
-        app.failure_message(CLI::FailureMessage::help);
-        app.footer([] {
-            std::string out;
-            out += "Sample document names:\n";
-            out += list_documents();
-            // Remove newline because the output looks too long.
-            out.erase(out.size() - 1);
-            return out;
-        });
+        // Prepare the argument list.
+        parser.addHelpOption();
+        parser.addVersionOption();
+        parser.addPositionalArgument("FILE", gtr("main", "Module file to open."));
 
-        try {
-            app.parse(argc, argv);
-        } catch (const CLI::ParseError &e) {
-            return ReturnCode{app.exit(e)};
+        auto sample_doc = QCommandLineOption(
+            "sample-doc",
+            gtr("main", "Name of sample document to load."),
+            gtr("main", "name"));
+
+        parser.addOption(sample_doc);
+
+        // Parse the arguments.
+        // May exit if invalid arguments, --help, or --version is passed.
+        if (!parser.parse(arguments)) {
+            bail_help(parser, QStringLiteral("%1: %2").arg(
+                gtr("main", "error"),
+                parser.errorText()));
+        }
+        if (parser.isSet(QStringLiteral("version"))) {
+            // Exits the program.
+            parser.showVersion();
+        }
+        if (parser.isSet(QStringLiteral("help"))) {
+            // Prints the default help text, followed by a list of sample document names.
+            help_and_exit(parser);
+        }
+        if (parser.isSet(QStringLiteral("help-all"))) {
+            // Prints the default app+Qt help text.
+            parser.process(arguments);
+            // This should be unreachable, since parser should exit upon seeing
+            // --help-all.
+            abort();
         }
 
-        return Arguments{.doc_name=doc_name};
+        Arguments out;
+        auto positional = parser.positionalArguments();
+        if (0 < positional.size()) {
+            out.filename = positional[0];
+        }
+        if (1 < positional.size()) {
+            bail_help(parser, gtr("main", "Too many command-line arguments, expected FILE"));
+        }
+
+        if (parser.isSet(sample_doc)) {
+            out.sample_doc = parser.value(sample_doc).toStdString();
+        }
+
+        // Validate the arguments.
+        if (has(out.sample_doc) && has(out.filename)) {
+            bail_only(gtr("main", "Cannot pass both --sample-doc <%1> and FILE.")
+                .arg(sample_doc.valueName()));
+        }
+
+        return out;
     }
 };
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    using gui::app::GuiApp;
+
     QGuiApplication::setHighDpiScaleFactorRoundingPolicy(
         Qt::HighDpiScaleFactorRoundingPolicy::RoundPreferFloor
     );
-    gui::app::GuiApp a(argc, argv);
+    GuiApp a(argc, argv);
+    QCoreApplication::setApplicationName("exotracker");
+    // Don't call QGuiApplication::setApplicationDisplayName()
+    // to append the app name to windows.
+    // It can't be turned off for instrument dialogs,
+    // uses hyphens on Windows but en dashes on Linux (and you can't tell which),
+    // and disappears when a file or instrument is named ExoTracker.
 
     // Parse command-line arguments.
-    auto maybe_arg = Arguments::parse(argc, argv);
-    if (auto return_code = std::get_if<ReturnCode>(&maybe_arg)) {
-        return return_code->value;
-    }
-    auto arg = std::get<Arguments>(std::move(maybe_arg));
-
-    bool has_doc_name = !arg.doc_name.empty();
+    // May exit if invalid arguments, --help, or --version is passed.
+    auto arg = Arguments::parse_or_exit(QCoreApplication::arguments());
 
     using sample_docs::DOCUMENTS;
-    if (has_doc_name && !DOCUMENTS.count(arg.doc_name)) {
-        fmt::print(
-            stderr, "Invalid document name \"{}\". Valid names are:\n\n", arg.doc_name
+    if (has(arg.sample_doc) && !DOCUMENTS.count(arg.sample_doc)) {
+        fmt::print(stderr,
+            "Invalid document name \"{}\". Valid names are:\n\n", arg.sample_doc
         );
-        fmt::print("{}", list_documents());
+        fmt::print(stderr, "{}", list_documents());
         return 1;
     }
     auto document = EXPR(
-        if (has_doc_name) {
-            return sample_docs::DOCUMENTS.at(arg.doc_name).clone();
+        if (has(arg.filename)) {
+            bail_only("TODO implement file path loading");
+        } else if (has(arg.sample_doc)) {
+            return sample_docs::DOCUMENTS.at(arg.sample_doc).clone();
         } else {
             return sample_docs::new_document();
         }
