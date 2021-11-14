d8114a0a6d65bd64e7a7d0ad5bd7d2c2a454c4f0
nyanpasu64 <nyanpasu64@tuta.io>
2021-11-13 17:46:14 -0800

    Switch PersistentDialog to QWidget (wrong title bar on Windows)

--- src/gui/lib/persistent_dialog.cpp
+++ src/gui/lib/persistent_dialog.cpp
@@ -4,28 +4,17 @@
 
 #include <QKeyEvent>
 #include <QKeySequence>
-#include <QPushButton>
 
 namespace gui::lib::persistent_dialog {
 
 W_OBJECT_IMPL(PersistentDialog)
 
 PersistentDialog::PersistentDialog(QWidget * parent)
-    : QDialog(parent)
+    : QWidget(parent)
 {
     setAttribute(Qt::WA_DeleteOnClose);
     setWindowFlag(Qt::Dialog);
     setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
-}
-
-void PersistentDialog::setVisible(bool visible) {
-    if (visible) {
-        QList<QPushButton *> const list = findChildren<QPushButton*>();
-        for (QPushButton * pb : list) {
-            pb->setAutoDefault(false);
-        }
-    }
-    QDialog::setVisible(visible);
 }
 
 void PersistentDialog::keyPressEvent(QKeyEvent * e) {
--- src/gui/lib/persistent_dialog.h
+++ src/gui/lib/persistent_dialog.h
@@ -2,30 +2,17 @@
 
 #include <verdigris/wobjectdefs.h>
 
-#include <QDialog>
+#include <QWidget>
 
 namespace gui::lib::persistent_dialog {
 
-/// A dialog type intended for persistent non-modal editor windows.
-///
-/// Unlike QDialog, it doesn't have a "default button" triggered upon pressing Enter.
-///
-/// Unlike QWidget, on Win32, it's properly centered above the parent window and
-/// (as expected) has no minimize/maximize buttons.
-///
-/// I couldn't fix some problems shared among both QDialog and QWidget.
-/// On KWin, clicking the parent window (which raises all dialogs) reorders the dialogs
-/// by first-created on top, rather than preserving the stacking order.
-/// And on all OSes, closing a dialog activates the parent window rather than the
-/// previously-active dialog.
-class PersistentDialog : public QDialog {
+class PersistentDialog : public QWidget {
     W_OBJECT(PersistentDialog)
 public:
     explicit PersistentDialog(QWidget * parent = nullptr);
 
 // impl QWidget
 protected:
-    void setVisible(bool visible) override;
     void keyPressEvent(QKeyEvent * e) override;
 };
 
