e2dbb9e78fdebf5efd3e6a18b81f88e797a9f42d
nyanpasu64 <nyanpasu64@tuta.io>
2021-12-15 22:23:18 -0800

    Cleanup PersistentDialog

--- src/gui/lib/persistent_dialog.cpp
+++ src/gui/lib/persistent_dialog.cpp
@@ -13,8 +13,8 @@ W_OBJECT_IMPL(PersistentDialog)
 PersistentDialog::PersistentDialog(QWidget * parent)
     : QDialog(parent)
 {
+    // TODO remove call, and have dialog creator set Qt::WA_DeleteOnClose
     setAttribute(Qt::WA_DeleteOnClose);
-    setWindowFlag(Qt::Dialog);
     setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
 }
 
@@ -29,10 +29,16 @@ void PersistentDialog::setVisible(bool visible) {
 }
 
 void PersistentDialog::keyPressEvent(QKeyEvent * e) {
+    // Calls reject() if Escape is pressed. Ignore other keypresses.
     if (e->matches(QKeySequence::Cancel)) {
-        close();
+        reject();
+    } else {
+        e->ignore();
     }
 
+    // We don't want Enter presses to trigger the default button (even though there
+    // should be no default button because we called setAutoDefault(false)). To be
+    // safe, don't call QDialog::keyPressEvent().
 }
 
 } // namespace
